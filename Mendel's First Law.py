c5b1c8b229809483c5176f69c130cb8bf14ae4ad
onat-the-wanderer <71891536+onat-the-wanderer@users.noreply.github.com>
2023-01-16 16:56:48 +0300

    Add files via upload

---
+++ Mendel's First Law.py
@@ -0,0 +1,18 @@
+def mendel():
+    dir=input("type direction")
+    with open(dir, "r") as f:
+        (k,m,n)=(int(value) for value in f.readline().split())
+    a=k+m+n
+    num=(4*(k*(k-1)+2*k*m+2*k*n+m*n)+3*m*(m-1))/(4*a*(a-1))
+    file=open("result.txt","w+")
+    file.write(str(round(num,5)))
+    file.close()
+
+    return (print(round(num,5)))
+
+
+
+mendel()
+
+
+#/Users/onat/Downloads/rosalind_iprb.txt
