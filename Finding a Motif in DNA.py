3de8a0c6a6ce5180e98168f4dfdc7d9441eff05b
onat-the-wanderer <71891536+onat-the-wanderer@users.noreply.github.com>
2023-05-03 18:18:16 +0300

    Update Finding a Motif in DNA.py

--- Finding a Motif in DNA.py
+++ Finding a Motif in DNA.py
@@ -1,22 +1,23 @@
-#dir=input("type directory")
-dir="/Users/onat/Downloads/rosalind_subs.txt"
+#"/Users/onat/Downloads/rosalind_subs-2.txt"
+dir=input("type directory")
 with open(dir, "r") as f:
     f=f.read()
        
-f=f.split("\n")   
+f=f.split("\n")         # s, t = f.read().split()
 string="".join(f[0])
 substring="".join(f[1])
 location=[]
+locationasstring=""
 
 def finding_motif(string):
     for i in range(len(string)):
         if string[i:].startswith(substring):
             location.append(i+1)
     file=open("result.txt", "w+")
-    file.write("".join(str(location)))
+    locationasstring="".join(str(location))
+    locationasstring=locationasstring.replace("[","").replace("]", "").replace(",", " ")
+    file.write(locationasstring)
     file.close      
     return 
     
 finding_motif(string)
-
-
